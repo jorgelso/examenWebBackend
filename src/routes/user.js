97ee5b5bd9e79f3647232bffaa8a48e3d42914cd
Jorge Salcedo <165866373+jorgelso@users.noreply.github.com>
2025-04-28 12:43:51 -0600

    endpoint login

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,10 +7,19 @@ const UserController = require('../controllers/user');
 
 const userService = UserServiceFactory.create('fake');
 const userController = new UserController(userService);
-
 const userHandler = new UserHttpHandler(userController);
 
 router.get('/', userHandler.getAllUsers.bind(userHandler));
-router.post('/login', userHandler.login.bind(userHandler));
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await userController.login(username, password);
+    res.json({ success: true, user });
+  } catch (error) {
+    res.status(401).json({ success: false, message: error.message });
+  }
+});
 
 module.exports = router;
